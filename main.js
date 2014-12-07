fea6fb6a48eed13b0eec508d60dcd11d1c7b0525
Bert Hartmann <BertHartm@gmail.com>
2014-12-07 01:27:39 -0500

    Add a Lists service

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 var listMod = angular.module('listMod', []);
 listMod.factory('List', [function() {
-    var List = function() {
+    var List = function(name) {
+        this.name = name;
         this.items = [];
     };
     List.prototype.addItem = function(val) {
@@ -13,9 +14,22 @@ listMod.factory('List', [function() {
     };
     return List;
 }]);
-
-listMod.controller('WishlistCtrl', ['$scope', 'List', function($scope, List) {
-    $scope.list = new List();
+listMod.service('Lists', ['List', function(List) {
+    var lists = [];
+    this.getLists = function() {
+        return lists;
+    };
+    this.addList = function(name) {
+        if (!name) {
+            throw new Error("List needs a name");
+        }
+        lists.push(new List(name));
+    };
+}]);
+listMod.controller('WishlistCtrl', ['$scope', 'Lists', function($scope, Lists) {
+    Lists.addList("my-list");
+    Lists.addList("your-list");
+    $scope.lists = Lists.getLists();
 }]);
 
 var wishlist = angular.module('wishlist', ['ngRoute', 'listMod']).
